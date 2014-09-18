6cff0b0c1bdb07cdfadd19a35335134071e0e661
Linards Jukmanis <Linards.Ju@gmail.com>
2014-09-19 01:22:25 +0300

    Automatic calculation function for samples of 2^N size

--- LD2/Hadamard transform.c
+++ LD2/Hadamard transform.c
@@ -7,14 +7,12 @@
 uint16_t binRev( uint16_t num, uint8_t length )
 {
   uint16_t rev_num = 0, count = length;
-
   while(num){
-    rev_num  |= num & 0x01;
     rev_num <<= 1;
+    rev_num  |= num & 0x01;
     num >>= 1;
     count--;
   }
-
   return rev_num << count;
 }
 
@@ -30,9 +28,36 @@ uint16_t HadamarToWalsh( uint16_t num, uint8_t length )
   return binToGray(binRev(num, length));
 }
 
+// WORKS ONLY WITH 2^N LONG SIGNALS!!!!
+uint16_t FWHT( int16_t *signal_arr, uint16_t signal_length)
+{
+  uint8_t stages = 0;
+  signal_length--;
+  while( signal_length ){
+    stages++;
+    signal_length >>= 1;
+  }
+
+  uint16_t tempstage, i, k, half, id, upper, downer;
+   
+  for(tempstage = stages; tempstage>0; tempstage--){
+      half = 1 << (tempstage-1);    
+      for(k=0; k<(1<<(stages-tempstage)); k++)
+        for(i=0; i<half; i++){
+          id = i + k*(1<<(tempstage-1));
+          upper  = signal_arr[id];
+          downer = signal_arr[id+half];
+          //printf("upper: %d, downer: %d, id: %d\n", upper, downer, id);
+          signal_arr[id] = upper + downer;
+          signal_arr[id+half] = -upper + downer;
+        }
+   }
+   return 0;
+}
+
 int main(void)
 {
-  int8_t is[4] = {0, 1, 2, 5};
+  int16_t is[4] = {0, 1, 2, 5};
   int8_t iresult[4] = {0};
   int8_t result[4] = {0};
   int8_t f[4][4] = {  1,  1,  1,  1,
@@ -61,6 +86,10 @@ int main(void)
   }
 
   printf("out %d, %d, %d, %d\n", out_s[0], out_s[1], out_s[2], out_s[3]);
+
+  
+  FWHT(is, 4);
+  printf("k   %d, %d, %d, %d\n", is[0], is[1], is[2], is[3]);
   return;
 }
 
