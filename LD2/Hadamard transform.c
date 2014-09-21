49b39c6809f448b9dc80bb844ffe4edd357d0215
Linards Jukmanis <Linards.Ju@gmail.com>
2014-09-21 16:24:10 +0300

    Work in progress, Hadamar Matrix generator done in a more usable form

--- LD2/Hadamard transform.c
+++ LD2/Hadamard transform.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Reverses the bits of a number (1101 -> 1011; 1010 -> 0101 etc.)
 // num - the number to be converted
 // length - the amount of bits in the number
-uint16_t binRev( uint16_t num, uint8_t length )
+uint16_t binRev(uint16_t num, uint8_t length)
 {
   uint16_t rev_num = 0, count = length;
   while(num){
@@ -17,16 +19,46 @@ uint16_t binRev( uint16_t num, uint8_t length )
 }
 
 // Converts regular number to it's correlating Gray code value
-uint16_t binToGray( uint16_t num )
+uint16_t binToGray(uint16_t num)
 {
   return (num >> 1) ^ num;
 }
 
 // For rearranging coefficients from the Hadamar order to the Walsh order
-uint16_t HadamarToWalsh( uint16_t num, uint8_t length )
+uint16_t HadamarToWalsh(uint16_t num, uint8_t length)
 {
   return binToGray(binRev(num, length));
 }
+
+// Create a Hadamar matrix
+int8_t **HadamarMatrix(uint16_t order)
+{
+  int8_t **matrix = calloc(1<<order, sizeof(int8_t*));
+  uint8_t i, stage, block_size, block_edge, x, y;
+  for(i = 0; i<(1<<order); i++)
+    matrix[i] = calloc(1<<order, sizeof(int8_t));
+  matrix[0][0] = 1;
+  for(stage = 0; stage<order; stage++){
+    block_edge = (1<<stage);
+    for(x = 0; x<block_edge; x++)
+      for(y = 0; y<block_edge; y++){
+        matrix[x+block_edge][y] = matrix[x][y];
+        matrix[x][y+block_edge] = matrix[x][y];
+        matrix[x+block_edge][y+block_edge] = -matrix[x][y]; 
+      }
+  }
+  return matrix;
+}
+
+// Free a Hadamar Matrix memory
+void freeHadamarMatrix(int8_t **matrix, uint16_t block_edge)
+{
+  uint16_t x;
+  for(x = 0; x<block_edge; x++)
+    free(matrix[x]);
+  free(matrix);
+}
+
 
 // WORKS ONLY WITH 2^N LONG SIGNALS!!!!
 uint16_t FWHT( int16_t *signal_arr, uint16_t signal_length)
@@ -38,19 +70,27 @@ uint16_t FWHT( int16_t *signal_arr, uint16_t signal_length)
     signal_length >>= 1;
   }
 
-  uint16_t tempstage, i, k, half, id, upper, downer;
-   
+  int16_t tempstage, i, k, half, id, upper, downer;
+  int16_t block_amount;
   for(tempstage = stages; tempstage>0; tempstage--){
-      half = 1 << (tempstage-1);    
-      for(k=0; k<(1<<(stages-tempstage)); k++)
+      half = 1 << (tempstage - 1);            // Half of the block length   
+      block_amount = 1<<(stages - tempstage); // The amount of blocks
+      printf("Half: %d, block_amount: %d\n", half, block_amount);
+      for(k=0; k<block_amount; k++){
         for(i=0; i<half; i++){
-          id = i + k*(1<<(tempstage-1));
+          id = i + k*half*2;
           upper  = signal_arr[id];
           downer = signal_arr[id+half];
-          //printf("upper: %d, downer: %d, id: %d\n", upper, downer, id);
+          printf("upper: %d, downer: %d, id: %d\n", upper, downer, id);
+          int8_t iter;
           signal_arr[id] = upper + downer;
-          signal_arr[id+half] = -upper + downer;
+          signal_arr[id+half] = upper - downer;
         }
+        int8_t iter;
+        for(iter = 0; iter<(4); iter++)
+          printf("% 2d", signal_arr[iter]);
+        printf("\n");
+      }
    }
    return 0;
 }
@@ -72,10 +112,10 @@ int main(void)
   iresult[2] = -is[2] + is[0];
   iresult[3] = -is[3] + is[1];
   
-  result[HadamarToWalsh(0, 2)] =  iresult[0] + iresult[1];
-  result[HadamarToWalsh(1, 2)] = -iresult[1] + iresult[0];
-  result[HadamarToWalsh(2, 2)] =  iresult[2] + iresult[3];
-  result[HadamarToWalsh(3, 2)] = -iresult[3] + iresult[2];
+  result[0] =  iresult[0] + iresult[1];
+  result[1] = -iresult[1] + iresult[0];
+  result[2] =  iresult[2] + iresult[3];
+  result[3] = -iresult[3] + iresult[2];
 
   printf("k   %d, %d, %d, %d\n", result[0],result[1],result[2],result[3]);
   
@@ -88,8 +128,27 @@ int main(void)
   printf("out %d, %d, %d, %d\n", out_s[0], out_s[1], out_s[2], out_s[3]);
 
   
+
+  uint8_t order = 2; 
+  int8_t **matrix = HadamarMatrix(order);
+  uint16_t x, y, block_edge;
+  int16_t result1[4] = {0};
+  block_edge = 1<<order;
+  for(y = 0; y<block_edge; y++){
+    for(x = 0; x<block_edge; x++)
+      printf("% 2d", matrix[x][y]);
+    printf("\n");
+  }
+
   FWHT(is, 4);
-  printf("k   %d, %d, %d, %d\n", is[0], is[1], is[2], is[3]);
-  return;
+  printf("out %d, %d, %d, %d\n", is[0], is[1], is[2], is[3]);
+  for(x = 0; x<4; x++)
+    for(y = 0; y<4; y++)
+      result1[x] += matrix[x][y]*is[y];
+  printf("out %d, %d, %d, %d\n", result1[0], result1[1], result1[2], result1[3]);
+  //return;
+  
+
+  freeHadamarMatrix(matrix, block_edge);
 }
 
